6adb6dad4edcc8c17dfb3eec07baccf2217ebbe1
romy <romy@snowyla.com>
2024-09-03 15:50:25 -0700

    udpated

---
+++ swiftPlayground.playground/Pages/AdjacencyMatrix.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+import Foundation
+
+/*:
+ 
+# Graph Representation
+ 
+
+![graph whiteboard](whiteboard1.jpeg)
+ 
+ From Leetcode there is a very nice description of graphs and representations
+ at in their interview crash course page on [Trees and graphs](https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4721/)
+ */
+
+/*:
+ 
+# Ways of Representing Graphs
+ 
+The ea
+ */
+
+
+// here is a graph with no connections
+var isolated: [[Int]] = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+
+/*:
+ 
+ However, the end result makes for much more natural Swift code.
+ 
+ ![plague doctor](Plague.jpg)
+ */
+//: [Next: Representation](Representation)
--- swiftPlayground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ swiftPlayground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -3,34 +3,31 @@ import Foundation
 
 /*:
  
-# Graph Representation
+# Introduction: Coffee and Algorithms
  
-
-![graph whiteboard](whiteboard1.jpeg)
+This is the structure Swift Playground notebook that will tell a story of graphs.
+ Also, it is better to learn in context with many connections, as we don't want to
+ flush this knowledge down the chute once anyone gets a job after the
+ algorithms interview hazing is through. 
+ 
+ * [Coffee and Algorithms: The Plague](https://sudoroom.org/events/coffee-algorithms-social-networks-and-revolution-2/)
  
  From Leetcode there is a very nice description of graphs and representations
  at in their interview crash course page on [Trees and graphs](https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4721/)
- */
-
-/*:
- 
-# Ways of Representing Graphs
- 
-The ea
- */
 
 
-// here is a graph with no connections
-var isolated: [[Int]] = [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1]
-]
-
-/*:
- 
- However, the end result makes for much more natural Swift code.
  
  ![plague doctor](Plague.jpg)
  */
+//: [Next: Representation](Representation)
+
+/*:
+ 
+* Karger’s Algorithm - [Geeks for Geeks Implementation](https://www.geeksforgeeks.org/introduction-and-implementation-of-kargers-algorithm-for-minimum-cut/)
+* [The Plague Network](https://blogs.cornell.edu/info2040/2019/09/23/the-plague-network/) – Cornell University - trade networks that helped spread the plague
+* ​Leper Colonies – [Wikipedia](https://en.wikipedia.org/wiki/Leper_colony)
+* Boccacio’s the Decameron [Brown University's Decameron Web](https://www.brown.edu/Departments/Italian_Studies/dweb/plague/) – we couldn’t leave this week without a spice of literature 🙂
+ 
+ */
+
 //: [Next: Representation](Representation)
