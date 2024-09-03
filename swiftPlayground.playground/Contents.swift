eb1ae4b5487390776022aeb7315bb70fd1fd350e
romy <romy@snowyla.com>
2024-09-03 13:47:00 -0700

    coffee and algorithms

---
+++ swiftPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+import Foundation
+/*:
+ 
+ # SudoRoom
+ 
+ Social networks at the Plague
+ The entire code to implement proposal is only a handful of lines of Swift:
+ 
+ ## Representation of Graphs is up to you!
+
+*/
+
+/*:
+ 
+ However, the end result makes for much more natural Swift code.
+ 
+ ![plague doctor](Plague.jpg)
+ */
