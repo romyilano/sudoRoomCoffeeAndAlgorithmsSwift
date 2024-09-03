6ac1eced1fda0dc156d16d8a548844555f9d20b0
romy <romy@snowyla.com>
2024-09-03 14:46:41 -0700

    updated

---
+++ swiftPlayground.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import Foundation
+//: [Representation](Representation)
+/*:
+ 
+# Graph Representation
+ 
+ ![graph whiteboard](whiteboard1.jpeg)
+ 
+ From Leetcode there is a very nice description of graphs and representations
+ at in their interview crash course page on [Trees and graphs](https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/707/traversals-trees-graphs/4721/)
+ */
+
+/*:
+ 
+# Ways of Representing Graphs
+ 
+
+ */
+
+
+// here is a graph with no connections
+var isolated: [[Int]] = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+
+/*:
+ 
+ However, the end result makes for much more natural Swift code.
+ 
+ ![plague doctor](Plague.jpg)
+ */
+ 
