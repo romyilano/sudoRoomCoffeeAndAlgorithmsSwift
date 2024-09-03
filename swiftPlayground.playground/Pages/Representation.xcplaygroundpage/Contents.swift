04d8ef627b550bc2f85e78d716e72d219ff3a8c3
romy <romy@snowyla.com>
2024-09-03 16:41:14 -0700

    updated the edges list

--- swiftPlayground.playground/Pages/Representation.xcplaygroundpage/Contents.swift
+++ swiftPlayground.playground/Pages/Representation.xcplaygroundpage/Contents.swift
@@ -13,12 +13,71 @@ import Foundation
  
  * there are `n` cities labeled from `0` to `n-1`
  * sometimes they won't even mention it's a graph and you have to figure that out for yourself
+ 
+ 
  */
 
 
+// graphs as an array of directed edges
+var edges = [[0, 1],
+             [1,2],
+             [2,0],
+             [2,3]
+]
+/*:
+ 
+ ## Showing graphs as an array of edges
+ 
+ ![list of edges](graph_list_edges.jpeg)
+ 
+ > Note that the graph in the image doesn't exist in memory. It only exists as an array ```
+ [[0, 1], [1, 2], [2, 0], [2, 3]]
+ ```
+ */
+// to build the graph from an array of edges you can create a hash
 
- /*:
- ![Whiteboard](whiteboard1.jpeg)
+func build_graph(edges: [[Int]], directed: Bool = false) -> [Int: [Int]] {
+    var graph = [Int: [Int]]()
+    for edge in edges {
+        // this is more of tuple
+        guard edge.count == 2 else {
+            continue
+        }
+        print(edge)
+        print(edge[0])
+        var x = edge[0]
+        var y = edge[1]
+        if graph[x] == nil {
+            graph[x] = [Int]()
+        }
+        graph[x]?.append(edge[1])
+        // if it's undreicted add the y
+      
+        if directed {
+            if graph[y] == nil {
+                graph[y] = [Int]()
+            }
+            graph[y]?.append(x)
+        }
+        // graph[edge[1]].append(edge[0])
+    }
+    return graph
+}
+
+let graph = build_graph(edges: edges, directed: true)
+print(graph)
+
+/*:
+ 
+ This example will reconstruct as a hash map of all the nodes (vertices)
+ and their edges:
+ ```
+ [2: [1, 0, 3],
+ 0: [1, 2],
+ 1: [0, 2], 3: [2]]
+ ```
+ 
+ ![list of edges](graph_list_edges.jpeg)
  
  */
 
